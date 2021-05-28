205db07724a70c61bc6ac422e6a1405bc842d466
RahmaYasser <rahma.y9079@gmail.com>
2021-05-28 17:46:15 +0200

    adding controllers and subscriber routes

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,6 @@
 class Subscriber < ApplicationRecord
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 }, format: { with:VALID_EMAIL_REGEX }
 
 end
